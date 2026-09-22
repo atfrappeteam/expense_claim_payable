aa8faa9d71e680b6e45c8b3a1a4b43212657cde6
rahulkul7218 <kulkarnirahul134@gmail.com>
2026-09-22 17:59:50 +0530

    update

--- expense_claim_payable/expense_claim_payable/api/expense_claim.py
+++ expense_claim_payable/expense_claim_payable/api/expense_claim.py
@@ -17,22 +17,22 @@
 
 
 
-import frappe
-from frappe import _
-from frappe.utils import flt
+# import frappe
+# from frappe import _
+# from frappe.utils import flt
 
 
-def validate_expense_claim(doc, method):
-	for row in doc.get("expenses"):
-		# Amount must always be greater than 0
-		if flt(row.get("amount")) <= 0:
-			frappe.throw(
-				_("Row #{0}: Amount must be greater than 0").format(row.idx)
-			)
+# def validate_expense_claim(doc, method):
+# 	for row in doc.get("expenses"):
+# 		# Amount must always be greater than 0
+# 		if flt(row.get("amount")) <= 0:
+# 			frappe.throw(
+# 				_("Row #{0}: Amount must be greater than 0").format(row.idx)
+# 			)
 
-		# Sanctioned Amount is not required for Rejected Expense Claims
-		if doc.get("status") != "Rejected":
-			if flt(row.get("sanctioned_amount")) <= 0:
-				frappe.throw(
-					_("Row #{0}: Sanctioned Amount must be greater than 0").format(row.idx)
-				)
+# 		# Sanctioned Amount is not required for Rejected Expense Claims
+# 		if doc.get("status") != "Rejected":
+# 			if flt(row.get("sanctioned_amount")) <= 0:
+# 				frappe.throw(
+# 					_("Row #{0}: Sanctioned Amount must be greater than 0").format(row.idx)
+# 				)
